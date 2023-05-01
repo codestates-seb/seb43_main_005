707970c14e0d635064a6b081e4cdcd0c10beed0c
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-05-01 13:56:28 +0900

    init : 초기파일 세팅

---
+++ client/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-uses-react": 0,
+  },
+};
---
+++ client/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: "es5", // 꼬리 콤마 사용 안함
+  tabWidth: 2, // 들여쓰기
+  semi: true, // 문장 끝 세미콜론
+  singleQuote: false, // 작은 따옴표
+  bracketSpacing: true, // 중괄호 내에 공백 사용
+  bracketSameLine: true, //
+  arrowParens: "avoid", // 화살표 함수 인자 하나일 때 괄호 생략
+  printWidth: 80, // 한 줄 최대 문자 수
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+};
