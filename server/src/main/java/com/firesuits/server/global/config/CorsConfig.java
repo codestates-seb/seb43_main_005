bc014cd5fdb44cfd0c6cd6ad52586761a12c4189
Gonue <6017891@naver.com>
2023-05-04 21:59:42 +0900

    feat : Cors 설정
    현재는 모든 Origin, Http Method, Http header, 자격증명을 모두 허용하도록 구성. 추후 논의후 수정

---
+++ server/src/main/java/com/firesuits/server/global/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.firesuits.server.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setExposedHeaders(Arrays.asList("*"));
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**,", config);
+
+        return new CorsFilter(source);
+    }
+}
