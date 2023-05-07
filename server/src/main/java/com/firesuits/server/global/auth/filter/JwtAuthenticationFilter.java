5d69819b113859900af48e8e82353b8499843277
Gonue <6017891@naver.com>
2023-05-07 15:21:12 +0900

    feat : Token payload 정보 수정
    - 검증과정에서 username보단 email이 적합하다 판단하여 email로 수정

--- server/src/main/java/com/firesuits/server/global/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/com/firesuits/server/global/auth/filter/JwtAuthenticationFilter.java
@@ -61,7 +61,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     // Access Token
     private String delegateAccessToken(Member member) {
         Map<String, Object> claims = new HashMap<>();
-        claims.put("username", member.getEmail());
+        claims.put("email", member.getEmail());
         claims.put("roles", member.getRoles());
 
         String subject = member.getEmail();
