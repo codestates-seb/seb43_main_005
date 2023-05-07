63e44f4b3c7bcb4170dfce4cd6728180a570286a
Gonue <6017891@naver.com>
2023-05-07 17:12:06 +0900

    feat : Exception 처리 추가
    - GlobalException 세분화
    - ErrorResponse 정의
    - ExceptionCode 수정

--- server/src/main/java/com/firesuits/server/global/error/advice/GlobalExceptionAdvice.java
+++ server/src/main/java/com/firesuits/server/global/error/advice/GlobalExceptionAdvice.java
@@ -2,28 +2,99 @@ package com.firesuits.server.global.error.advice;
 
 
 import com.firesuits.server.global.error.exception.BusinessLogicException;
-import com.firesuits.server.global.error.exception.ExceptionCode;
-import com.firesuits.server.global.error.response.Response;
+import com.firesuits.server.global.error.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
-    @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity<?> applicationHandler(BusinessLogicException e){
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
         log.error("Error occurs {}",e.toString());
-        return ResponseEntity.status(e.getExceptionCode().getStatus())
-                .body(Response.error(e.getExceptionCode().name()));
+        final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getExceptionCode()
+                .getStatus()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> applicationHandler(RuntimeException e){
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleRuntimeException(RuntimeException e){
         log.error("Error occurs {}",e.toString());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Response.error(ExceptionCode.INTERNAL_SERVER_ERROR.name()));
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        log.error("Error occurs {}",e.toString());
+        final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(
+            ConstraintViolationException e) {
+        log.error("Error occurs {}",e.toString());
+        final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+        log.error("Error occurs {}",e.toString());
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException e) {
+        log.error("Error occurs {}",e.toString());
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,
+                "Required request body is missing");
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(
+            MissingServletRequestParameterException e) {
+        log.error("Error occurs {}",e.toString());
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.BAD_REQUEST,
+                e.getMessage());
+
+        return response;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.error("# handle Exception", e);
+        final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return response;
     }
 }
--- server/src/main/java/com/firesuits/server/global/error/exception/ExceptionCode.java
+++ server/src/main/java/com/firesuits/server/global/error/exception/ExceptionCode.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ExceptionCode {
 
-    DUPLICATED_EMAIL(409, "이메일을 찾을 수 없음"),
-    MEMBER_EXISTS(409, "이메일이 이미 존재"),
+    DUPLICATED_EMAIL(409, "이메일이 이미 존재"),
+    MEMBER_EXISTS(409, "멤버가 이미 존재"),
     MEMBER_NOT_FOUND(404,"멤버를 찾을 수 없음"),
     ARTICLE_NOT_FOUND(404, "게시물을 찾을 수 없음"),
     INVALID_PERMISSION(403,"권한이 없습니다."),
