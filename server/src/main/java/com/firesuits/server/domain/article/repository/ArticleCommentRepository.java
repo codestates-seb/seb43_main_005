2f698bd7db297951cb9ac8f32036b3a1167547f8
Gonue <6017891@naver.com>
2023-05-12 00:07:31 +0900

    feat : MyCommentList 추가
    - 내가 작성한 댓글 기능 추가

--- server/src/main/java/com/firesuits/server/domain/article/repository/ArticleCommentRepository.java
+++ server/src/main/java/com/firesuits/server/domain/article/repository/ArticleCommentRepository.java
@@ -2,6 +2,7 @@ package com.firesuits.server.domain.article.repository;
 
 import com.firesuits.server.domain.article.entity.Article;
 import com.firesuits.server.domain.article.entity.ArticleComment;
+import com.firesuits.server.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
     Page<ArticleComment> findAllByArticle(Article article, Pageable pageable);
+    Page<ArticleComment> findAllByMember(Member member, Pageable pageable);
 }
