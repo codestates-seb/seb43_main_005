19ea43e564a96cd87757f96e7266b8df2102d88a
Hyun Jin, Lee <nagi5924@gmail.com>
2023-05-12 11:34:46 +0900

    [Fix] mypage btn added

--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import PageContainer from "../common/PageContainer.jsx";
 import Profile from "../components/Mypage/Profile.jsx";
 import Level from "../components/Mypage/Level.jsx";
+import CustomButton from "../common/CustomButton.jsx";
 
 export default function Mypage() {
   return (
@@ -10,15 +11,22 @@ export default function Mypage() {
         <Profile />
         <Level />
       </MyContainer>
+      <BtnContainer>
+        <CustomButton text="대시보드" />
+        <CustomButton text="내가 쓴 토론 글" />
+        <CustomButton text="설정" />
+      </BtnContainer>
     </PageContainer>
   );
 }
 
 const MyContainer = styled.main`
-  background-color: aliceblue;
   width: 100%;
   display: flex;
   @media ${props => props.theme.mediaQuery.mobile} {
     flex-direction: column;
   }
+`;
+const BtnContainer = styled.article`
+  background-color: aliceblue;
 `;
