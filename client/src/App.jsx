89fc6a3f7d9200edd6f372f529edf33d08797366
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-05-16 15:58:08 +0900

    feat : header course ver

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,9 @@ import Course from "./pages/Course.jsx";
 
 function App() {
   const { pathname } = useLocation();
-  const hideHeaderFooter = pathname.startsWith("/user");
+  const hideHeaderFooter =
+    pathname.startsWith("/user") || /^\/course\/\w/.test(pathname);
+  // path가 '/user'로 시작하거나, '/course/문자혹은숫자' 로 시작하면 common header 숨김
 
   return (
     <ThemeProvider theme={theme}>
