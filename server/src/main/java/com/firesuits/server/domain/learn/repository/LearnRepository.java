8c513cbdf784b968e5b323b720e308093c9a1fc4
Yoon-JM <wlals3591@gmail.com>
2023-05-17 14:36:20 +0900

    feat: Learn Response 수정

--- server/src/main/java/com/firesuits/server/domain/learn/repository/LearnRepository.java
+++ server/src/main/java/com/firesuits/server/domain/learn/repository/LearnRepository.java
@@ -2,13 +2,23 @@ package com.firesuits.server.domain.learn.repository;
 
 import com.firesuits.server.domain.content.entity.Content;
 import com.firesuits.server.domain.learn.entity.Learn;
-import com.firesuits.server.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LearnRepository extends JpaRepository<Learn, Long> {
     Page<Learn> findAllByContent(Content contentBoard, Pageable pageable);
+    @Query("SELECT c FROM Learn c WHERE c.content.id = :contentId")
+    List<Learn> findByContentId(Long contentId);
+
+//    @Query("SELECT COUNT(c) FROM Learn c WHERE c.content.id = :contentId")
+//    Integer countByLearn(Long contentId);
+
+    @Query("SELECT COUNT(c) FROM Learn c WHERE c.content.id = :contentId")
+    Integer countByContentId(Long contentId);
 }
