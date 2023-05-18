3f1c1734c401ca0ce09d806a9f0857f4527c6dd3
soulhn <vldnrlf123@naver.com>
2023-05-18 18:36:49 +0900

    feat: O,X 퀴즈 결과에 따른 결과 화면, 색상 처리 추가

--- client/src/components/OxQuiz/OxQuiz.jsx
+++ client/src/components/OxQuiz/OxQuiz.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CustomProgressBar from "../common/CustomProgressBar.jsx";
 import O from "../../assets/images/O.png";
 import X from "../../assets/images/X.png";
+import resultImg from "../../assets/images/resultImg.png";
 
 function OxQuiz(props) {
+  const [isFinished, setisFinished] = useState(false); // 초기 상태를 체크된 상태로 설정
   return (
     <QuizContainer>
-      <h2>OX퀴즈</h2>
+      {isFinished ? (
+        <>
+          <h2>테스트 결과</h2>
+          <StyledImage src={resultImg} alt="resultImg"></StyledImage>
+          <Quiz>9 / 10 점</Quiz>
+        </>
+      ) : (
+        <>
+          <CustomProgressBar progress={80} feat={"simple"} />
+          <h2>OX퀴즈</h2>
 
-      {/* progress, marginBottom, feat */}
-      <CustomProgressBar progress={80} feat={"simple"} />
-      <Quiz>자바스크립트는 객체 지향 언어이다.</Quiz>
-      <AnswerContainer>
-        <Answer>
-          <StyledImage src={O} alt="O"></StyledImage>
-        </Answer>
-        <Answer>
-          <StyledImage src={X} alt="X"></StyledImage>
-        </Answer>
-      </AnswerContainer>
+          <Quiz>자바스크립트는 클래스 기반 객체 지향 언어이다.</Quiz>
+
+          <AnswerContainer>
+            <Answer highlighted={true}>
+              <StyledImage src={O} alt="O"></StyledImage>
+            </Answer>
+            <Answer highlighted={false}>
+              <StyledImage src={X} alt="X"></StyledImage>
+            </Answer>
+          </AnswerContainer>
+          <QuizSolution>
+            <p>정답 : O</p>
+            <br />
+            <br />
+            <br />
+            <p>
+              풀이 : 자바스크립트는 프로토타입 기반 객체 지향 프로그래밍
+              언어입니다.
+            </p>
+          </QuizSolution>
+        </>
+      )}
     </QuizContainer>
   );
 }
@@ -29,9 +51,8 @@ export default OxQuiz;
 const QuizContainer = styled.div`
   padding: 50px;
   box-sizing: border-box;
-  /* border: 1px solid black; */
-  max-width: 1020px;
-  width: 90%;
+  max-width: 1125px;
+  width: 100%;
   margin: 0 auto;
   > h2 {
     margin-bottom: 30px;
@@ -39,8 +60,9 @@ const QuizContainer = styled.div`
 `;
 
 const Quiz = styled.div`
-  border: 1px solid black;
-  width: 1028px;
+  border: ${({ theme }) => theme.color.borderLight};
+  border-radius: 8px;
+  width: 100%;
   height: 126px;
   display: flex;
   justify-content: center;
@@ -52,20 +74,36 @@ const AnswerContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  width: 1028px;
-  /* border: 3px solid blue; */
+  width: 100%;
+  margin-bottom: 50px;
 `;
 
 const Answer = styled.div`
   width: 500px;
   height: 222px;
-  border: 1px solid black;
+  border: ${({ highlighted, theme }) =>
+    highlighted ? `1px solid ${theme.color.blue}` : theme.color.borderLight};
+  border-radius: 8px;
+  box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const StyledImage = styled.img`
-  width: 200px;
-  height: 200px;
+  width: 150px;
+  height: 150px;
+`;
+
+const QuizSolution = styled.div`
+  width: 100%;
+  height: 222px;
+  border: ${({ theme }) => theme.color.borderLight};
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 50px;
 `;
