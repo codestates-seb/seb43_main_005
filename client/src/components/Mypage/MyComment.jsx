b10bcab74f81fb06aba54f2ca877628b5824782e
Hyun Jin, Lee <nagi5924@gmail.com>
2023-05-19 14:13:19 +0900

    [Fix] fix

--- client/src/components/Mypage/MyComment.jsx
+++ client/src/components/Mypage/MyComment.jsx
@@ -4,23 +4,27 @@ import { getComment } from "../../api/apiUtil.js";
 import useEffect from "react";
 
 export default function MyComment() {
-  // 내가 작성한 토론글 목록 get
-  // function myCommentCheck() {
-  //   getComment({ page: 1, size: 10, sort: "default" }, "/members/my-comment")
-  //     .then(res => console.log(res))
-  //     .catch(error => console.log(error));
-  // }
+  // 내가 작성한 토론글 목록 get => 임시, 버튼 누르면 불러오기
+  // 불러온대로 페이지네이션으로 보여주기
+  // 정렬 : 버튼 누르면 sort 별로 요청하기
+  function myCommentCheck() {
+    getComment({ page: 5, size: 10, sort: "likes" }, "/members/my-comment")
+      .then(res => {
+        console.log(res.result);
+      })
+      .catch(error => console.log(error));
+  }
   // useEffect(() => {
   //   myCommentCheck();
   // }, []);
 
-  // 임시 랜덤이미지
+  // 임시 랜덤이미지 => 전역 상태에서 받아오기
   let userProfileImage = "https://source.unsplash.com/random/300x300/?animal";
   // DebateTitle 클릭 시 해당 토론글로 이동하기
   return (
     <>
       <DebateContainer className="DebateContainer">
-        {/* <button onClick={myCommentCheck}>불러오기</button> */}
+        <button onClick={myCommentCheck}>불러오기</button>
         <CommentContainer>
           <ProfileImage profileImg={userProfileImage} feat="mycomment" />
           <CommentBox>
