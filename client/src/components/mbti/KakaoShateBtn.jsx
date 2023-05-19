0fc51173b12f64669a6766a2e13f3d149d6a38a9
soulhn <vldnrlf123@naver.com>
2023-05-20 00:10:34 +0900

    feat: MBTI 카카오톡 공유하기

--- client/src/components/mbti/KakaoShateBtn.jsx
+++ client/src/components/mbti/KakaoShateBtn.jsx
@@ -1,97 +1,54 @@
-// import React, { useEffect } from "react";
-// import kakaoIcon from "../../assets/images/icon_sns_kakao.svg";
-// import styled from "styled-components";
-// // const { Kakao } = window;
-// function KakaoShateBtn(props) {
-//   const url = "";
-//   const resultUrl = window.location.href;
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import kakaoIcon from "../../assets/images/icon_sns_kakao.svg";
+const { Kakao } = window;
+function KakaoShateBtn({ resultData }) {
+  const url = process.env.REACT_APP_HOSTING_URL;
+  const testUrl = url + "/mbti";
+  const resultUrl = window.location.href;
+  console.log(window.location.href);
 
-//   useEffect(() => {
-//     window.Kakao.init("77f0bbf6099f6b081f907b40197fbd9f");
-//     // window.Kakao.isInitialized();
-//   }, []);
+  useEffect(() => {
+    if (!Kakao.isInitialized()) {
+      Kakao.init(process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY);
+    }
+  }, []);
 
-//   window.Kakao.Link.sendDefault({
-//     objectType: "feed",
-//     content: {
-//       title: "오늘의 디저트",
-//       description: "아메리카노, 빵, 케익",
-//       imageUrl:
-//         "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
-//       link: {
-//         mobileWebUrl: "https://developers.kakao.com",
-//         webUrl: "https://developers.kakao.com",
-//       },
-//     },
-//     itemContent: {
-//       profileText: "Kakao",
-//       profileImageUrl:
-//         "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-//       titleImageUrl:
-//         "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-//       titleImageText: "Cheese cake",
-//       titleImageCategory: "Cake",
-//       items: [
-//         {
-//           item: "Cake1",
-//           itemOp: "1000원",
-//         },
-//         {
-//           item: "Cake2",
-//           itemOp: "2000원",
-//         },
-//         {
-//           item: "Cake3",
-//           itemOp: "3000원",
-//         },
-//         {
-//           item: "Cake4",
-//           itemOp: "4000원",
-//         },
-//         {
-//           item: "Cake5",
-//           itemOp: "5000원",
-//         },
-//       ],
-//       sum: "총 결제금액",
-//       sumOp: "15000원",
-//     },
-//     social: {
-//       likeCount: 10,
-//       commentCount: 20,
-//       sharedCount: 30,
-//     },
-//     buttons: [
-//       {
-//         title: "웹으로 이동",
-//         link: {
-//           mobileWebUrl: "https://developers.kakao.com",
-//           webUrl: "https://developers.kakao.com",
-//         },
-//       },
-//       {
-//         title: "앱으로 이동",
-//         link: {
-//           mobileWebUrl: "https://developers.kakao.com",
-//           webUrl: "https://developers.kakao.com",
-//         },
-//       },
-//     ],
-//   });
+  const handleClick = () => {
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "CODE TRAVELER MBTI 결과",
+        description: resultData.mbti,
+        imageUrl: url + resultData.image,
+        link: {
+          mobileWebUrl: resultUrl,
+          webUrl: resultUrl,
+        },
+      },
+      buttons: [
+        {
+          title: "MBTI 테스트 해보기",
+          link: {
+            webUrl: testUrl,
+            mobileWebUrl: testUrl,
+          },
+        },
+      ],
+    });
+  };
 
-//   return <KakaoStyleBtn />;
-// }
+  return <KakaoStyleBtn onClick={handleClick}></KakaoStyleBtn>;
+}
+const KakaoStyleBtn = styled.button`
+  background: url(${kakaoIcon}) no-repeat center center;
+  border: 1px solid black;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
 
-// export default KakaoShateBtn;
-
-// const KakaoStyleBtn = styled.button`
-//   background: url(${kakaoIcon}) no-repeat center center;
-//   border: 1px solid black;
-//   width: 60px;
-//   height: 60px;
-//   border-radius: 30px;
-
-//   :hover {
-//     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-//   }
-// `;
+  :hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  }
+`;
+export default KakaoShateBtn;
