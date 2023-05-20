dc1bc7343bc8a7548727911673f79c33bb82c2df
Hyun Jin, Lee <nagi5924@gmail.com>
2023-05-20 15:25:35 +0900

    [Design] locked UI added

--- client/src/components/Mypage/ThemeCircle.jsx
+++ client/src/components/Mypage/ThemeCircle.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import themes from "../../assets/styels/theme.jsx";
+import lock from "../../assets/images/lock.png";
 
-export default function ThemeCircle({ eachTheme, onClick }) {
+export default function ThemeCircle({ eachTheme, onClick, opened }) {
   const eTheme = themes[eachTheme];
   return (
     <>
-      <ThemeImg theme={eTheme} onClick={onClick}></ThemeImg>
+      {opened ? (
+        <ThemeImg theme={eTheme} onClick={onClick}></ThemeImg>
+      ) : (
+        <LockedImg />
+      )}
     </>
   );
 }
@@ -22,5 +27,18 @@ const ThemeImg = styled.div`
   :hover {
     transform: scale(1.05);
     transition-duration: 0.5s;
+  }
+`;
+const LockedImg = styled(ThemeImg)`
+  width: 124px;
+  height: 124px;
+  border: none;
+  background-color: ${props => props.theme.gray50};
+  background-image: url(${lock});
+  background-position: center;
+  background-repeat: no-repeat;
+  cursor: default;
+  :hover {
+    transform: none;
   }
 `;
--- client/src/components/Mypage/Themes.jsx
+++ client/src/components/Mypage/Themes.jsx
@@ -1,15 +1,21 @@
 import styled from "styled-components";
 import ThemeCircle from "./ThemeCircle.jsx";
+import { useState } from "react";
 
 export default function Themes() {
   const handleThemeChange = value => {
     // 유저가 바꾼 테마를 서버로 보내서 저장하기 => 임시로 로컬 스토리지에 저장
     localStorage.setItem("theme", value);
-    console.log(`저장 : ${value}`);
+    // console.log(`저장 : ${value}`);
     // 다시 가져와서 상태 업데이트하기
     // const savedTheme = localStorage.getItem("theme");
     // setSelectedTheme(savedTheme);
   };
+
+  // 유저의 경험치가 해당 레벨에 도달하지 않았을 경우 : 기본 설정으로 자물쇠 이미지 넣기
+  // 유저 인포 가져와서 경험치로 레벨 확인하고나서 opened 여부 내려주기?
+  const [opened, setOpened] = useState(true);
+
   const themeList = ["default", "ocean", "desert", "forest", "space", "pet"];
 
   return (
@@ -24,14 +30,16 @@ export default function Themes() {
                 value={`${el}Light`}
                 onClick={() => handleThemeChange(`${el}Light`)}
                 role="none"
-                eachTheme={`${el}Light`}>
+                eachTheme={`${el}Light`}
+                opened={opened}>
                 {`${el}Light`}
               </ThemeCircle>
               <ThemeCircle
                 value={`${el}Dark`}
                 onClick={() => handleThemeChange(`${el}Dark`)}
                 role="none"
-                eachTheme={`${el}Dark`}>
+                eachTheme={`${el}Dark`}
+                opened={opened}>
                 {`${el}Dark`}
               </ThemeCircle>
             </Theme>
