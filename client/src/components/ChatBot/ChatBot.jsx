b7539e4a45fd18075b7d0f23af4ac8cbe8f0f174
soulhn <vldnrlf123@naver.com>
2023-05-23 03:00:44 +0900

    feat: chatbot 1.0 업로드

--- client/src/components/ChatBot/ChatBot.jsx
+++ client/src/components/ChatBot/ChatBot.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 //이미지를 불러옵니다.
 import chatbot from "../../assets/images/chatbot.png";
 import chatbotClose from "../../assets/images/chatbotClose.png";
+import imgShip from "../../assets/images/imgShip.png";
 
 function ChatBot(props) {
   const [resultData, setResultData] = useState({}); //최종 결과 데이터
@@ -38,7 +39,7 @@ function ChatBot(props) {
           const id = Object.keys(updatedResult).length + 1; // 새로운 아이디 생성
           updatedResult[id] = [{ question: inputData, answer: response }];
           setResultData(updatedResult);
-          setInput(""); // clear the input field
+          setInput("");
         });
     } catch (error) {
       console.log("ChatBot 에러 발생");
@@ -51,13 +52,17 @@ function ChatBot(props) {
       </ModalBtn>
       {openChatBot && (
         <ModalContainer>
-          <ModalHeader>Code Travler 챗봇</ModalHeader>
+          <ModalHeader>
+            <img src={imgShip} alt="ChatBotLogo" />
+            <h2>CODE TRAVELER 학습 도우미</h2>
+          </ModalHeader>
           <ModalContent>
             {Object.keys(resultData).map(key => (
               <Fragment key={key}>
                 <UserQuestion>
                   <strong>Question:</strong> {resultData[key][0].question}
                 </UserQuestion>
+                {/* <img src={chatbot} alt="chatbotImg" /> */}
                 <ChatBotAnswer key={key}>
                   <strong>Answer:</strong> {resultData[key][0].answer}
                 </ChatBotAnswer>
@@ -66,7 +71,10 @@ function ChatBot(props) {
             <div ref={contentEndRef} />
           </ModalContent>
           <Form onSubmit={handleSubmit}>
-            <ModalInput value={inputData} onChange={handleChange}></ModalInput>
+            <ModalInput
+              placeholder={"질문을 입력해주세요."}
+              value={inputData}
+              onChange={handleChange}></ModalInput>
           </Form>
         </ModalContainer>
       )}
@@ -76,38 +84,58 @@ function ChatBot(props) {
 
 // 모달이 열렸을 때 보이는 페이지
 const ModalContainer = styled.button`
-  /* border: 1px solid black; */
-  border: ${({ theme }) => theme.borderBold};
+  border-radius: 15px;
+  overflow: hidden;
   width: 400px;
   height: 55%;
   background-color: ${props => props.theme.white};
   filter: drop-shadow(2px 2px 5px rgba(0, 0, 0, 0.3));
   display: flex;
-  /* align-items: center; */
   justify-content: center;
   position: fixed;
   right: 50px;
   bottom: 250px;
   flex-direction: column;
-  /* border-radius: 10px; */
+  z-index: 9999;
 `;
 
 const Form = styled.form`
-  display: contents;
-`;
-
-const ModalHeader = styled.div`
-  width: 100%;
-  height: 15%;
-  border: 1px solid red;
-  /* border-radius: ; */
-`;
-
-const ModalInput = styled.input`
+  padding: 10px;
   box-sizing: border-box;
   width: 100%;
   height: 15%;
-  border: ${({ theme }) => theme.borderBold};
+  display: flex;
+  border-radius: 15px;
+  border: none;
+  min-height: 55px;
+`;
+
+const ModalHeader = styled.div`
+  padding: 15px;
+  box-sizing: border-box;
+  background-color: ${props => props.theme.main};
+  width: 100%;
+  height: 15%;
+  display: flex;
+  font-family: "GmarketSansBold", cursive;
+  /* justify-content: center; */
+  align-items: center;
+
+  > img {
+    width: 70px;
+    margin-right: 15px;
+  }
+  > h2 {
+    color: ${props => props.theme.white};
+    font-size: 1.1em;
+  }
+`;
+
+const ModalInput = styled.input`
+  /* border-top: 1px solid black; */
+  /* border-radius: 15px; */
+  width: 100%;
+  min-height: 20px;
 `;
 
 const ModalContent = styled.div`
@@ -123,19 +151,25 @@ const ModalContent = styled.div`
 `;
 
 const UserQuestion = styled.div`
-  width: 80%;
-  border: 1px solid black;
+  /* width: 80%; */
+  border: ${({ theme }) => theme.borderBold};
   align-self: flex-end;
   border-radius: 10px;
   margin-bottom: 10px;
+  box-sizing: border-box;
+  padding: 8px;
+  text-align: right;
 `;
 
 const ChatBotAnswer = styled.div`
   align-self: flex-start;
-  width: 80%;
-  border: 1px solid black;
+  /* width: 80%; */
+  border: ${({ theme }) => theme.borderBold};
+  box-sizing: border-box;
   border-radius: 10px;
   margin-bottom: 10px;
+  padding: 8px;
+  text-align: left;
 `;
 
 // 모달 오픈 버튼
