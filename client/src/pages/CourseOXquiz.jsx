df6b3f58a566023ef6c4eb4da5c6b5f255dc14a4
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2023-05-23 12:04:32 +0900

    feat : 퀴즈 로그인 상태 추가

--- client/src/pages/CourseOXquiz.jsx
+++ client/src/pages/CourseOXquiz.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 import CustomSideBar from "../components/common/CustomSideBar.jsx";
 import Header from "../components/Header/Header.jsx";
 // import ContentArticle from "../components/ContentArticle.jsx";
@@ -7,9 +8,11 @@ import OxQuiz from "../components/OxQuiz/OxQuiz.jsx";
 
 function CourseOXquiz(props) {
   const [lnb, setLnb] = useState(false);
+  const { userInfo } = useSelector(state => state.user);
+
   return (
     <Container>
-      <Header course setLnb={setLnb} />
+      <Header course setLnb={setLnb} userInfo={userInfo} />
       <Body>
         {lnb && <CustomSideBar>사이드바 컴포넌트</CustomSideBar>}
         <OxQuiz></OxQuiz>
